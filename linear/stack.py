e0b80eb74e462f9116482f83cfb1ad75d4e0626d
zby0902 <zbyroger@gmail.com>
2018-08-13 14:47:40 +1000

    some application of stack

--- linear/stack.py
+++ linear/stack.py
@@ -15,3 +15,45 @@ class Stack:
     def size(self):
         return len(self.items) 
 
+
+def baseConverter(number,base):
+    """
+    A base converter method based on the stack class
+    """
+    if base > 16:
+        raise ValueError('The limit of base is hex or 16')
+    digits = '0123456789ABCDEF'
+    rem_stack = Stack()
+    while number > 0:
+        rem = number % base
+        rem_stack.push(rem)
+        number = number//base
+    num_string = ''
+    while not rem_stack.isEmpty():
+        num_string += str(digits[rem_stack.pop()])
+    return num_string
+
+def parChecker(symbolString):
+    """
+    A simple method checking the parentisises are balanced or not using stack
+    """
+   s = Stack() 
+   balanced = True
+   index = 0
+   while index < len(symbolString) and balanced:
+       symbol = symbolString[index]
+       if symbol in '([{':
+           s.push(symbol)
+        else:
+            top = s.top()
+            if not matcheds(top,symbol):
+                balanced =  False
+        index += 1
+    if balanced  and s.isEmpty():
+        return True
+    else:
+        return False
+def matches(openP,closeP):
+    opens = '([{'
+    closes = ')]}'
+    return opens.index(openP) == closes.index(closeP)
